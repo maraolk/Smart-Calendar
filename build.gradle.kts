894ae8a22fc1619205d3b849e8dc0d188b1268c0
Arina <makarovaas2008@gmail.com>
2025-03-27 16:39:46 +0300

    pupupu version 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
 	testImplementation("io.kotest:kotest-assertions-core:5.7.2")
 	testImplementation("io.mockk:mockk:1.13.16")
 	testImplementation("com.ninja-squad:springmockk:4.0.2")
-
 }
 
 
