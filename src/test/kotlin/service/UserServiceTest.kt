a7577aff01b8dce7e824c38589f486748ffc9bab
bolrob <belkavas78@mail.ru>
2025-03-27 16:29:37 +0300

    tests

--- src/test/kotlin/service/UserServiceTest.kt
+++ src/test/kotlin/service/UserServiceTest.kt
@@ -1,6 +1,7 @@
 package service
 
 import demo.calendar.dto.SingUpRequest
+import demo.calendar.dto.UserResponse
 import demo.calendar.entity.UserEntity
 import demo.calendar.exception.UserAlreadyRegisteredException
 import demo.calendar.repository.UserRepository
@@ -38,5 +39,18 @@ class UserServiceTest {
 
         exception.message shouldBe "User with this tg is already registered"
 
+    }
+    @Test
+    fun `Регистрация пользователя, когда пользователь правильно регистрируется`(){
+        val newRequest = SingUpRequest(
+            userName = "Ostin",
+            phone = "891234567829",
+            email = "PUPUPUpupunia@popatarakana",
+            tg = "@SIGMABOY",)
+        every { userRepository.findByTg(newRequest.tg) } returns null
+        every {userRepository.save(UserEntity(
+            username=newRequest.userName, phone=newRequest.phone, email=newRequest.email, tg=newRequest.tg))} answers{firstArg()}
+        val response = userService.registerUser(newRequest)
+        response shouldBe UserResponse(userName=newRequest.userName, phone=newRequest.phone, email=newRequest.email, tg=newRequest.tg)
     }
 }
